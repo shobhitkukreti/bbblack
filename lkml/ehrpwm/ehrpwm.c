bcc6be16ff691a7883437e8dd5a721b933595083
shobhitkukreti <shobhitkukreti@gmail.com>
2016-09-06 01:31:32 -0700

    Basic PWM. Cape BB-PWM0

--- lkml/ehrpwm/ehrpwm.c
+++ lkml/ehrpwm/ehrpwm.c
@@ -13,79 +13,83 @@
 #include <linux/platform_device.h>
 #include <linux/pwm.h>
 
+#include "pwm.h"
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("skukreti");
 MODULE_DESCRIPTION("WS2812B Driver with TI PWM");
 
+#define PWM_IRQ 86
 
-#define PINMUX_BASE_ADDR 	 0x44e10000
-#define PWMSS_START_ADDRESS  0x48300000
-#define PWMSS_END_ADDRESS    0x483000FF
-#define EHRPWM_START_ADDRESS 0x48300200
-#define EHRPWM_END_ADDRESS   0x4830025F
+struct ehrpwm_struct {
+struct platform_device *pdev;
+struct resource *r_mem;
+void __iomem *pwm_base;
+unsigned int virq;
+}epwm;
 
-
-
-struct pwmsubsys {
-	struct pwm_chip ti_pwm;
-	unsigned int data;
-	struct platform_device *pdev;
-}pwm_data;
-
-
-
-void pwm_cleanup(struct device *dev)
+static int  pwm_probe ( struct platform_device *pdev ) 
 {
-	pr_info("PWM Release Function \n");
+	uint16_t reg=0;
+	printk(KERN_INFO"PWM_PROBE\n");
 
-}
-
-int  pwm_probe ( struct platform_device *pdev ) 
-{
-	struct resource *r_mem_pwmss, *r_mem_ehrpwm; 
-
-	pr_info("PWM0 Probe Called\n");
-	r_mem_pwmss = platform_get_resource (pdev, IORESOURCE_MEM, 0);
-
-	pr_info("1\n");
-	if(!r_mem_pwmss) 
-		pr_err("Resource MEM PWMSS not available \n");
-
-	r_mem_ehrpwm = platform_get_resource (pdev, IORESOURCE_MEM, 1);
+	epwm.r_mem = platform_get_resource (pdev, IORESOURCE_MEM, 0);
 	pr_info("2\n");
 
-	if(!r_mem_ehrpwm) 
+	if(!epwm.r_mem) 
 		pr_err("Resource MEM EHRPWM not available \n");
 
 
 	/* Check if IO Memory is available for use by the driver */
 
-	if((request_mem_region(r_mem_pwmss->start, r_mem_pwmss->end - \
-						r_mem_pwmss->start +1 , "EHRPWM_LED"))) {
-		pr_err (" %s, PWMSS Memory  Used by Anoter Driver\n", __func__);
+	if(!(request_mem_region(epwm.r_mem->start, resource_size(epwm.r_mem),"WS2812B")))
+	{
+		pr_err ("%s, EHRPWM Memory  Used by Anoter Driver\n",__func__);
 	
 	}
-	pr_info("3\n");
+	epwm.pwm_base = ioremap_nocache(epwm.r_mem->start, resource_size(epwm.r_mem));
 
-	if((request_mem_region(r_mem_ehrpwm->start, r_mem_ehrpwm->end - \
-						r_mem_ehrpwm->start +1 , "EHRPWM_LED"))) {
-		pr_err (" %s, EHRPWM Memory  Used by Anoter Driver\n", __func__);
+//	epwm.virq = irq_create_mapping (NULL, PWM_IRQ);
+//	printk( KERN_INFO "EPWM0 IRQ Mapped to %d\n", epwm.virq);	
+
+	/* TBCTL Register */
+	reg = 1;
+	reg |= 3<<7;
+	reg |= 3<<10; 
+	iowrite16(reg, epwm.pwm_base + TBCTL);
+	iowrite16(0XFFFF, epwm.pwm_base + TBCNT);
+	iowrite16(0x00, epwm.pwm_base + CMPCTL); /* Default value */
+	reg =0;
+	iowrite16(0xFF00, epwm.pwm_base + CMPA);
+	iowrite16(0xFF00, epwm.pwm_base +CMPB);
 	
-	}
+	/* Set Action Qualifier */
+	reg =1;
+	reg |= 3<<6;
+	iowrite16(reg, epwm.pwm_base + AQCTLB);
+		
 
-	pr_info("4\n");
+	printk(KERN_INFO"All is good \n");
 
-	pr_info("All is good \n");
-
-	pwm_data.pdev = pdev;
 	return 0;
 }
 
-int pwm_remove (struct platform_device *pdev)
+static int pwm_remove (struct platform_device *pdev) 
 {
-	pr_info("PWM Remove\n");
-	return 0;
+
+iounmap(epwm.pwm_base);
+release_mem_region(epwm.r_mem->start, resource_size(epwm.r_mem));
+printk(KERN_INFO"SK,PWM-DEV Remove");
+
+return 0;
 }
+
+static const struct of_device_id pwm_match[]={
+        {.compatible = "sk,pwm-dev", },
+        {},
+};
+
+MODULE_DEVICE_TABLE(of, pwm_match);
 
 
 struct platform_driver pwm_driver = {
@@ -93,61 +97,27 @@ struct platform_driver pwm_driver = {
 	.probe = &pwm_probe,
 	.remove = &pwm_remove,
 	.driver = {
-		.name = "sk, pwm-dev",
+		.name = "sk,pwm-dev",
+		.of_match_table = of_match_ptr(pwm_match),
 	
 	},
 
 };
 
 
+static int __init pwm_init(void) {
 
-static struct resource pwmsubsys0_resource[] = {
-	{
-		.start = PWMSS_START_ADDRESS,
-		.end   = PWMSS_END_ADDRESS,
-		.flags = IORESOURCE_MEM,
-	},
-	{
-		.start = EHRPWM_START_ADDRESS,
-		.end = EHRPWM_END_ADDRESS,
-		.flags = IORESOURCE_MEM,
-	}
-
-};
-
-
-static struct platform_device pwm_device = {
-	.name="sk, pwm-dev",
-	.id = 0,
-	.dev = {
-			.platform_data = &pwm_data,
-			.release = pwm_cleanup,
-	},
-	.resource = pwmsubsys0_resource,
-	.num_resources = ARRAY_SIZE(pwmsubsys0_resource),
-	
-	
-};
-
-
-static int __init pwm_init(void)
-{
-	int ret = 0;
-	ret = platform_device_register(&pwm_device);
-	if(ret < 0) 
-		pr_info(" PWM Device Reg failed\n");
-	ret = platform_driver_register(&pwm_driver);
-	if(ret < 0) 
-		pr_info(" PWM Driver Reg failed\n");
-
-return ret;
+        int ret=0;
+	printk(KERN_INFO"PWM_INIT"); 
+        ret=platform_driver_register(&pwm_driver);
+        return ret;
 }
 
 
 static void __exit pwm_exit(void) 
 {
+	printk(KERN_INFO"PWM_EXIT\n");
 	platform_driver_unregister(&pwm_driver);
-	platform_device_unregister(&pwm_device);
 
 }
 
