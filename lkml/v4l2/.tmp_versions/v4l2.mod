9d35c4f697f0d8a8d683345c9970043d8a4d6539
shobhit kukreti <shobhit.k1@samsung.com>
2018-11-16 11:29:14 -0800

    Revert "first commit for fake v4l2 driver"
    This reverts commit c13243c5cc05640adcc7a8267ffaa991a3a7632d.

--- lkml/v4l2/.tmp_versions/v4l2.mod
+++
@@ -1,2 +0,0 @@
-/home/t-1000/project/repo/kernel_modules/lkml/v4l2/v4l2.ko
-/home/t-1000/project/repo/kernel_modules/lkml/v4l2/v4l2.o
--- lkml/v4l2/.v4l2.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/t-1000/project/repo/kernel_modules/lkml/v4l2/v4l2.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/t-1000/project/repo/kernel_modules/lkml/v4l2/v4l2.ko /home/t-1000/project/repo/kernel_modules/lkml/v4l2/v4l2.o /home/t-1000/project/repo/kernel_modules/lkml/v4l2/v4l2.mod.o
